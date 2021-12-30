705c936e931c764e439cc5644d62061d4e352803
SC <pch838811@gmail.com>
2021-12-30 17:29:31 +0900

    설정변경

--- _config.js
+++ _config.js
@@ -16,14 +16,14 @@ module.exports = {
   /** Home > Bio information*/
   comment: 'Back-end developer',
   name: 'SeungCheol',
-  company: '',
+  //company: '',
   location: 'Korea',
   email: 'pch838811@gmail.com',
   website: 'https://pch8388.github.io',           // ex.'https://junhobaik.github.io'
-  linkedin: '',                                                          // ex.'https://www.linkedin.com/in/junho-baik-16073a19ab'
-  facebook: '',                                                          // ex.'https://www.facebook.com/zuck' or 'https://www.facebook.com/profile.php?id=000000000000000'
-  instagram: '',                                                         // ex.'https://www.instagram.com/junhobaik'
-  github: '',                                                            // ex.'https://github.com/junhobaik'
+  //linkedin: '',                                                          // ex.'https://www.linkedin.com/in/junho-baik-16073a19ab'
+  //facebook: '',                                                          // ex.'https://www.facebook.com/zuck' or 'https://www.facebook.com/profile.php?id=000000000000000'
+  //instagram: '',                                                         // ex.'https://www.instagram.com/junhobaik'
+  github: 'https://github.com/pch8388',                                                            // ex.'https://github.com/junhobaik'
 
   /** Post */
   enablePostOfContents: true,     // TableOfContents activation (Type of Value: Boolean. Not String)
