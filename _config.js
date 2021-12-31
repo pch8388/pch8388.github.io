b7d1c67a6048ef68bbccfebb6cac4ee026e2f0ab
SC <pch838811@gmail.com>
2022-01-01 04:09:31 +0900

    config

--- _config.js
+++ _config.js
@@ -25,10 +25,10 @@ module.exports = {
   enablePostOfContents: true,     // TableOfContents activation (Type of Value: Boolean. Not String)
   disqusShortname: 'pch8388-github-io',            // comments (Disqus sort-name)
   enableSocialShare: true,        // Social share icon activation (Type of Value: Boolean. Not String)
-
+  
   /** Optional */
   googleAnalytics: '',     // Google Analytics TrackingID. ex.'UA-123456789-0'
-  googleSearchConsole: '', // content value in HTML tag of google search console ownership verification. ex.'w-K42k14_I4ApiQKuVPbCRVV-GxlrqWxYoqO94KMbKo'
+  googleSearchConsole: 'yesM7ie0PFxkPcKblwnwqMAt8lqzwNKkhFqamEya_E0', // content value in HTML tag of google search console ownership verification. ex.'w-K42k14_I4ApiQKuVPbCRVV-GxlrqWxYoqO94KMbKo'
   googleAdsenseSlot: '',   // Google Adsense Slot. ex.'5214956675'
   googleAdsenseClient: '', // Google Adsense Client. ex.'ca-pub-5001380215831339'
     // Please correct the adsense client number(ex.5001380215831339) in the './static/ads.txt' file.
